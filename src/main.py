0d10fffcef6ee7c778bc9c1713a63ce727f64c3e
Mark Rapson <mrapson93@gmail.com>
2025-07-13 20:11:06 +0100

    Fix report depth and formatting

--- src/main.py
+++ src/main.py
@@ -3,11 +3,8 @@ from src.analyser.node_type import NodeType
 
 import argparse
 
-def print_tree(root, max_depth, max_breadth, depth = 0, prefix=""):
+def print_tree(root, max_depth, max_breadth, depth = 1, prefix=""):
     curr_breadth = 0
-    if depth == 0:
-        print(f"{root.name}/ ({root.get_size()})")
-
     for child in root.children:
         if child.get_type() == NodeType.FILE:
             if curr_breadth < max_breadth:
@@ -16,8 +13,8 @@ def print_tree(root, max_depth, max_breadth, depth = 0, prefix=""):
                 print(f"{prefix}|-- (...)")
             curr_breadth += 1
         elif child.get_type() == NodeType.DIRECTORY:
+            print(f"{prefix}|-- {child.name}/ ({child.get_size()})")
             if depth < max_depth:
-                print(f"{prefix}|-- {child.name}")
                 print_tree(child, max_depth, max_breadth, depth + 1, prefix=f"{prefix}|   ")
         else:
             print("unknown type")
@@ -25,7 +22,7 @@ def print_tree(root, max_depth, max_breadth, depth = 0, prefix=""):
 def main():
     arg_parser = argparse.ArgumentParser(prog="Disk Analyser")
     arg_parser.add_argument("--dir", required=True, help="Root folder to analyse")
-    arg_parser.add_argument("--depth", required=False, default=2, help="Maximum directory depth to print in output")
+    arg_parser.add_argument("--depth", required=False, default=1, help="Maximum directory depth to print in output")
     arg_parser.add_argument("--breadth", required=False, default=3, help="Maximum breadth of child items to print in output. Extra items will be truncated to (...)")
     args = arg_parser.parse_args()
 
@@ -34,7 +31,10 @@ def main():
 
     print("+-----------------------------------+")
     print(f"| Report for {args.dir}")
+    print(f"| Depth: {args.depth}")
+    print(f"| Breadth: {args.breadth}")
     print("+-----------------------------------+")
+    print(f"{root.name}/ ({root.get_size()})")
     print_tree(root, args.depth, args.breadth)
 
 
